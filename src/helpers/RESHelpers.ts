64b4b8bf5e358a99f3859fc7075a6ce04480a250
huanent <huanent@hotmail.com>
2017-04-13 13:25:50 +0800

    '新增画矩形帮助方法'

--- src/helpers/RESHelpers.ts
+++ src/helpers/RESHelpers.ts
@@ -2,8 +2,8 @@ class RESHelpers {
 
 	static createImg(name: string, width: number = null, height: number = null): egret.Bitmap {
 		let img = new egret.Bitmap(RES.getRes(name));
-		
-		img.visible=false;
+
+		img.visible = false;
 		if (width) img.width = width;
 		if (height) img.height = height;
 
@@ -13,7 +13,7 @@ class RESHelpers {
 	static createTxt(content: string, color: number = null, bgColor: number = null): egret.TextField {
 		let txt = new egret.TextField();
 
-		txt.visible=false;
+		txt.visible = false;
 		if (color) txt.textColor = color;
 		if (bgColor) txt.backgroundColor = bgColor;
 		txt.text = content;
@@ -21,5 +21,12 @@ class RESHelpers {
 		return txt;
 	}
 
+	static createShape(x: number, y: number, width: number, height: number, color: number, alpha: number): egret.Shape {
+		let shape = new egret.Shape();
+		shape.graphics.beginFill(color, alpha);
+		shape.graphics.drawRect(x, y, width, height);
+		shape.graphics.endFill();
+		return shape;
+	}
 
 }
